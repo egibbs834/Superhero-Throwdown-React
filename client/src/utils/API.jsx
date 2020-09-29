99379922dc459d34d2a575c9fbc13e3579ec8db5
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-09-29 13:24:14 -0400

    add api cv details

--- client/src/utils/API.jsx
+++ client/src/utils/API.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api_key = process.env.REACT_APP_API_KEY;
+const apicv_key = process.env.REACT.APP.APICV_KEY;
 
 console.log("api_key: ", api_key);
+console.log("apicv_key: ", apicv_key);
 
 export default {
   getSuperhero: function (name) {
@@ -11,4 +13,10 @@ export default {
       `${api_key}/search/${name}`
     );
   },
+  getMoreInfo: function (name) {
+    console.log("name", name);
+    return axios.get(
+      `${apicv_key}/search/${name}`
+    )
+  }
 };
