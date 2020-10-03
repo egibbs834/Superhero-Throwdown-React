c9db525ec46ad106c531e94510acf6d2a88f369c
Stovencrantz <sknapp143@gmail.com>
2020-10-02 23:06:58 -0400

    Added components for the universe page, universe page cards, and a frontCard and backCard for flipping animations. Displays all our heroes in a row and column format.

--- client/src/components/UniverseCard/index.jsx
+++ client/src/components/UniverseCard/index.jsx
@@ -1,14 +1,55 @@
-import React from "react"
-import { MDBCol, MDBCard, MDBCardBody, MDBCardText } from "mdbreact";
+import { render } from 'react-dom'
+import React, { useState } from 'react'
+import { useSpring, animated as a } from 'react-spring'
+import "./style.css"
+import {
+    MDBView,
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCol,
+    MDBRow,
+    MDBProgress,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+  } from "mdbreact";
+import "./style.css";
+import UniverseCardFront from "../UniverseCardFront";
+// import UniverseCardBack from "../UniverseCardBack";
 
-export default function UniverseCard(props) {
+  export default function UniverseCard(props) {
+    const [flipped, set] = useState(false)
+    const { transform, opacity } = useSpring({
+      opacity: flipped ? 1 : 0,
+      transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+      config: { mass: 5, tension: 500, friction: 80 }
+    })
+    console.log("universe card props: ", props)
+    return(
+        <div >
+            <MDBRow className="justify-content-center align-items-center row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 mx-5" id="rowWrapper">
+            {props.characters.map((character, i) => {
+                return (
+                    <MDBCol className = "mt-5">
+                        <UniverseCardFront character={character} increment={i} />
 
-    return (
-        <MDBCol>
-            <MDBCard>
-
-            </MDBCard>
-        </MDBCol>
+                        {/* <a.div className="c front"  onClick={() => set(state => !state)} style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
+                        Front Card component here
+                            <UniverseCardFront character={character} increment={i}/>
+                        </a.div> */}
+                        {/* <a.div className="c back" style={{ opacity, transform: transform.interpolate(t => `${t} rotateY(180deg)`) }}>
+                            Back Card component here 
+                             <UniverseCardBack character={character} />
+                        </a.div> */}
+                    </MDBCol>
+                )
+             })}
+            </MDBRow>
+        </div> 
     )
-}
-
+  }
--- client/src/pages/Universe/index.jsx
+++ client/src/pages/Universe/index.jsx
@@ -1,49 +1,82 @@
-import React, { useState, useEffect, useContext } from "react";
-import { MDBContainer, MDBCol, MDBRow, MDBCard, MDBCardBody, MDBCardText } from "mdbreact";
-import UniverseCard from "../../components/UniverseCard"
-import API from "../../utils/API"
+import React, { useState, useEffect, useContext,  } from "react";
+import {
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBView,
+  MDBJumbotron,
+  MDBAnimation, MDBMask
+} from "mdbreact";
+import UniverseCard from "../../components/UniverseCard";
+import API from "../../utils/API";
 import UsernameContext from "../../context/usernameContext";
+import "./style.css";
+import parallaxImg from "./images/blueVredCloudsNebula.jpg";
+import LoadingSpinner from "../../components/LoadSpinner";
+import { useSpring, animated as a } from 'react-spring'
 
 
 export default function Universe() {
+  const [heroes, setHeroes] = useState({});
+  const {username, setUsername} = useContext(UsernameContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [heroes, setHeroes] = useState({});
-    const { username, setUsername } = useContext(UsernameContext);
-    console.log("Universe username: ", username)
+  // Handles state for card flips
+  const [flipped, set] = useState(false)
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 }
+  })
 
-    function populateUniverse() {
-        console.log("populateUniverse username: ", username)
-        API.getAllHeroes(username)
-        .then((res) => {
-            console.log("AllHeroes Res: ", res.data);
-            setHeroes(res.data);
-        })
-        .catch(console.err)
-    }
 
-    // useEffect(() => {
-    //     console.log('trigger use effect hook');
-    //     populateUniverse()
+  console.log("Universe username: ", username);
 
-    //     // setTimeout(() => {
-    //     //   setMessage("I'm fine, thanks for asking.");
-    //     // }, 1000)
-    //   }, []);
-      
-    // console.log(`all heroes in universe for ${username}: ${heroes}`)
-    console.log("heroes: ", heroes)
-    return (
-        <MDBContainer>
-            <MDBRow className="justify-content-center row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5" id="rowWrapper">
-                <MDBCard>
-                    <MDBCardBody>
-                        <MDBCardText>
-                            We made it to the Universe
-                            <UniverseCard />
-                        </MDBCardText>
-                    </MDBCardBody>
-                </MDBCard>
+  function populateUniverse() {
+    console.log("populateUniverse username: ", username);
+    API.getAllHeroes(username)
+      .then((res) => {
+        console.log("AllHeroes Res: ", res.data);
+        setHeroes(res.data);
+        setIsLoading(false);
+
+      })
+      .catch(console.err);
+  }
+
+  useEffect(() => {
+    console.log("trigger use effect hook");
+    populateUniverse();
+    // setTimeout(() => {
+    //   setMessage("I'm fine, thanks for asking.");
+    // }, 1000)
+  }, []);
+
+  // console.log(`all heroes in universe for ${username}: ${heroes}`)
+  console.log("heroes: ", heroes);
+  return (
+    <div id="universePage">
+      {/* <MDBJumbotron>Welcome to our universe</MDBJumbotron> */}
+      <MDBView src={parallaxImg} fixed className="universeBackground">
+          <MDBMask overlay="grey-light" className="flex-center"></MDBMask>
+        <MDBContainer fluid className="mt-3">
+          {isLoading ? (
+            <MDBRow>
+                <MDBCol className = "text-center align-items-center">
+                    <LoadingSpinner />
+                </MDBCol>
             </MDBRow>
+            ) : (
+                <MDBAnimation type="fadeInDown" delay="2s">
+                    <UniverseCard characters={heroes} />
+                </MDBAnimation>            
+            )}
+
         </MDBContainer>
-    )
+      </MDBView>
+    </div>
+  );
 }
