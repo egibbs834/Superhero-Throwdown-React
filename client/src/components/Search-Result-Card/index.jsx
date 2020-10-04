b405de8ca43f0f3d42f73bbae888ebfc9ca87f26
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-10-04 15:05:39 -0400

    feat: add first comic fix: moreinfo updates each time search first changes

--- client/src/components/Search-Result-Card/index.jsx
+++ client/src/components/Search-Result-Card/index.jsx
@@ -22,11 +22,12 @@ import { useSpring, animated as a } from 'react-spring';
 import UsernameContext from "../../context/usernameContext";
 import HeroContext from "../../context/heroContext";
 import AddHeroModal from "../Modal/index";
+import LoadSpinner from "../LoadSpinner";
 
 const ResultCard = ({character}) => {
   console.log("(ResultCard) props: ", character);
   const[moreInfo,setMoreInfo] = useState(null);
-
+  const [loading, setLoading] = useState(true);
   const { username } = useContext(UsernameContext);
   const { heroContext, setHeroContext } = useContext(HeroContext);
   console.log("heroContext: ", heroContext);
@@ -51,9 +52,9 @@ const ResultCard = ({character}) => {
     }
   }
 
-useEffect(()=>{
-  getMoreInfo(character.name)
-},[])
+  useEffect(()=>{
+    getMoreInfo(character.name)
+  },[character])
 
 function getMoreInfo(name){
   API.getSuperheroID(name)
@@ -65,6 +66,7 @@ function getMoreInfo(name){
           .then((res3) => {
           console.log("res3:", res3);
           setMoreInfo(res3.data.results);
+          setLoading(false);
       })
     })
 }
@@ -75,155 +77,168 @@ moreInfo && console.log(moreInfo)
   //   console.log({ character });
   //   setHeroContext(character);
   // }
+
   return (
-              <MDBView hover zoom>
-                <MDBCard style={{ width: "16rem" }} className="m-2">
-                  <MDBCardImage
-                    className="img-thumbnail"
-                    src={character.img}
-                    style={{
-                      width: "100%",
-                      maxHeight: "338px",
-                    }}
-                    alt={`picture of ${character.name}`}
-                  />
-                  <MDBCardBody
-                    style={{
-                      padding: "none",
-                      textTransform: "capitalize",
-                    }}
-                  >
-                    <MDBCardTitle className="align-text-center myColor">
-                      <strong>{character.name}</strong>
-                    </MDBCardTitle>
-                    <hr></hr>
-                    <div className="flipCtn">
-                    <a.div className="moreInfoDiv" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`),willChange: "transform, opacity",postion:"absolute" }}>
-                      {moreInfo && <h6>Real Name:</h6>}
-                      {moreInfo && 
-                        <MDBCardText className="marginBtm mt-0">
-                          {moreInfo.real_name}
-                        </MDBCardText>
-                      }
-                      <hr></hr>
-                      {moreInfo && <h6>Powers:</h6>}
-                      {moreInfo && moreInfo.powers.slice(0,10).map((power, i) =>(
-                        <MDBCardText className="marginBtm mt-0" key={i}>
-                          {power.name}
-                        </MDBCardText>
-                      ))}
-                    </a.div>
-                    <a.div fluid style={{ opacity: opacity.interpolate(o => 1 - o), transform, willChange: "transform, opacity",position: "absolute"  }} onClick={()=>setFlipped(state => !state)}>
-                      <MDBCardText className="marginBtm mt-0">
-                        <strong>
-                          Tier Ranking:{" "}
-                          <span className="myColor">{character.tierList}</span>{" "}
-                        </strong>
-                      </MDBCardText>
-                      <MDBCardText>
-                        <strong>Total Power: {character.totalPower}</strong>{" "}
-                      </MDBCardText>
-                      <MDBCardText className="marginBtm">
-                        Alignment: {character.alignment}
-                      </MDBCardText>
-                      <MDBCardText className="marginBtm">
-                        Race: {character.race}
-                      </MDBCardText>
-                      <MDBCardText className="marginBtm">
-                        Height: {character.height}
-                      </MDBCardText>
-                      <MDBCardText className="marginBtm">
-                        Weight: {character.weight}{" "}
-                      </MDBCardText>
-                      <MDBCardText className="">
-                        Publisher: <strong>{character.publisher}</strong>
-                      </MDBCardText>
-                    </a.div>
-                    </div>
-                    <hr></hr>
-                    <MDBDropdown className="text-center" size="sm" hover dropup>
-                      <MDBDropdownToggle color="secondary">
-                        Power Stats
-                      </MDBDropdownToggle>
-                      <MDBDropdownMenu color="secondary" basic>
-                        <MDBDropdownItem>
-                          Combat: {character.combat}
-                          <MDBProgress
-                            className="my-2"
-                            material
-                            value={character.combat}
-                            color={statBarColor(parseInt(character.combat))}
-                          />
-                        </MDBDropdownItem>
-                        <MDBDropdownItem>
-                          Durability: {character.durability}
-                          <MDBProgress
-                            className="my-2"
-                            material
-                            value={character.durability}
-                            color={statBarColor(parseInt(character.durability))}
-                          />
-                        </MDBDropdownItem>
-                        <MDBDropdownItem>
-                          Intelligence: {character.intelligence}
-                          <MDBProgress
-                            className="my-2"
-                            material
-                            value={character.intelligence}
-                            color={statBarColor(
-                              parseInt(character.intelligence)
-                            )}
-                          />
-                        </MDBDropdownItem>
-                        <MDBDropdownItem>
-                          Power: {character.power}
-                          <MDBProgress
-                            className="my-2"
-                            material
-                            value={character.power}
-                            color={statBarColor(parseInt(character.power))}
-                          />
-                        </MDBDropdownItem>
-                        <MDBDropdownItem>
-                          Speed: {character.speed}
-                          <MDBProgress
-                            className="my-2"
-                            material
-                            value={character.speed}
-                            color={statBarColor(parseInt(character.speed))}
-                          />
-                        </MDBDropdownItem>
-                        <MDBDropdownItem>
-                          Strength: {character.strength}
-                          <MDBProgress
-                            className="my-2"
-                            material
-                            value={character.strength}
-                            color={statBarColor(parseInt(character.strength))}
-                          />
-                        </MDBDropdownItem>
-                      </MDBDropdownMenu>
-                    </MDBDropdown>
-                    <div className="row">
-                      <AddHeroModal character={character} username={username} />
-                    </div>
-                    <div className="row">
-                      <MDBContainer>
-                        <MDBBtn
-                          className="text-white"
-                          color="secondary"
-                          size="sm"
-                          onClick={() => setFlipped(state => !state)}
-                          href="#chart"
-                        >
-                          More Info
-                        </MDBBtn>
-                      </MDBContainer>
-                    </div>
-                    
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBView>      
+    <MDBView hover zoom>
+      
+      <MDBCard style={{ width: "16rem" }} className="m-2">
+        <MDBCardImage
+          className="img-thumbnail"
+          src={character.img}
+          style={{
+            width: "100%",
+            maxHeight: "338px",
+          }}
+          alt={`picture of ${character.name}`}
+        />
+        <MDBCardBody
+          style={{
+            padding: "none",
+            textTransform: "capitalize",
+          }}
+        >
+          <MDBCardTitle className="align-text-center myColor">
+            <strong>{character.name}</strong>
+          </MDBCardTitle>
+          <hr></hr>
+          <div className="flipCtn">
+          <a.div className="moreInfoDiv" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`),willChange: "transform, opacity",postion:"absolute" }}>
+          {loading ? <LoadSpinner/> :
+           <>
+            {moreInfo && <h8>Real Name:</h8>}
+            {moreInfo && 
+              <MDBCardText className="marginBtm mt-0">
+                {moreInfo.real_name}
+              </MDBCardText>
+            }
+            <hr></hr>
+            {moreInfo && <h8>First Commic:</h8>}
+            {moreInfo && 
+              <MDBCardText className="marginBtm mt-0">
+                {moreInfo.first_appeared_in_issue.name}
+              </MDBCardText>
+            }
+             <hr></hr>
+            {moreInfo && <h8>Powers:</h8>}
+            {moreInfo && moreInfo.powers.slice(0,10).map((power, i) =>(
+              <MDBCardText className="marginBtm mt-0" key={i}>
+                {power.name}
+              </MDBCardText>
+            ))}
+           </>
+          }
+          </a.div>
+          <a.div fluid style={{ opacity: opacity.interpolate(o => 1 - o), transform, willChange: "transform, opacity",position: "absolute"  }} onClick={()=>setFlipped(state => !state)}>
+            <MDBCardText className="marginBtm mt-0">
+              <strong>
+                Tier Ranking:{" "}
+                <span className="myColor">{character.tierList}</span>{" "}
+              </strong>
+            </MDBCardText>
+            <MDBCardText>
+              <strong>Total Power: {character.totalPower}</strong>{" "}
+            </MDBCardText>
+            <MDBCardText className="marginBtm">
+              Alignment: {character.alignment}
+            </MDBCardText>
+            <MDBCardText className="marginBtm">
+              Race: {character.race}
+            </MDBCardText>
+            <MDBCardText className="marginBtm">
+              Height: {character.height}
+            </MDBCardText>
+            <MDBCardText className="marginBtm">
+              Weight: {character.weight}{" "}
+            </MDBCardText>
+            <MDBCardText className="">
+              Publisher: <strong>{character.publisher}</strong>
+            </MDBCardText>
+          </a.div>
+          </div>
+          <hr></hr>
+          <MDBDropdown className="text-center" size="sm" hover dropup>
+            <MDBDropdownToggle color="secondary">
+              Power Stats
+            </MDBDropdownToggle>
+            <MDBDropdownMenu color="secondary" basic>
+              <MDBDropdownItem>
+                Combat: {character.combat}
+                <MDBProgress
+                  className="my-2"
+                  material
+                  value={character.combat}
+                  color={statBarColor(parseInt(character.combat))}
+                />
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                Durability: {character.durability}
+                <MDBProgress
+                  className="my-2"
+                  material
+                  value={character.durability}
+                  color={statBarColor(parseInt(character.durability))}
+                />
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                Intelligence: {character.intelligence}
+                <MDBProgress
+                  className="my-2"
+                  material
+                  value={character.intelligence}
+                  color={statBarColor(
+                    parseInt(character.intelligence)
+                  )}
+                />
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                Power: {character.power}
+                <MDBProgress
+                  className="my-2"
+                  material
+                  value={character.power}
+                  color={statBarColor(parseInt(character.power))}
+                />
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                Speed: {character.speed}
+                <MDBProgress
+                  className="my-2"
+                  material
+                  value={character.speed}
+                  color={statBarColor(parseInt(character.speed))}
+                />
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                Strength: {character.strength}
+                <MDBProgress
+                  className="my-2"
+                  material
+                  value={character.strength}
+                  color={statBarColor(parseInt(character.strength))}
+                />
+              </MDBDropdownItem>
+            </MDBDropdownMenu>
+          </MDBDropdown>
+          <div className="row">
+            <AddHeroModal character={character} username={username} />
+          </div>
+          <div className="row">
+            <MDBContainer>
+              <MDBBtn
+                className="text-white"
+                color="secondary"
+                size="sm"
+                onClick={() => setFlipped(state => !state)}
+                href="#chart"
+              >
+                More Info
+              </MDBBtn>
+            </MDBContainer>
+          </div>
+          
+        </MDBCardBody>
+      </MDBCard>
+    </MDBView>     
   );
 };
 export default ResultCard;
